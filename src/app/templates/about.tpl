tradle/craigslist-on-chain

commit 17c395fa68929e7418cb5d188b7f8de88365c0b8

Made Git revision less invasive
The git revision number on the about page isn't so invasive anymore. Doesn't add anything to package.json so it won't constantly cause merge conflicts!

src/app/templates/about.tpl
@@ -7,8 +7,8 @@
 	<div class="content">
 		<div class="title-version">
 			<%= App.settings.version %> Beta
-			<% if(gui.App.manifest['git']) { %>
-				- <small><i><%= gui.App.manifest.git.branch + ' (' + gui.App.manifest.git.commit.slice(0,8) + ')' %></i></small>
+			<% if(App.git) { %>
+				- <small><i><%= App.git.branch %> (<a class="links" href="https://git.popcorntime.io/stash/projects/PT/repos/popcorn-app/commits/<%= App.git.commit %>"><%= App.git.commit.slice(0,8) %></a>)</i></small>
 			<% } %>
 		</div>
 		<div class="text-about">
@@ -18,13 +18,13 @@
 			</div>
 		</div>
 		<div class="icons_social">
-			<a href='http://popcorntime.io' data-toggle="tooltip" data-placement="top" title="http://popcorntime.io" class='links site_icon'></span></a>
-			<a href='http://twitter.com/PopcornTimeTV' data-toggle="tooltip" data-placement="top" title="twitter.com/PopcornTimeTV" class='links twitter_icon'></span></a>
-			<a href='http://www.fb.com/PopcornTimeTV' data-toggle="tooltip" data-placement="top" title="fb.com/PopcornTimeTV" class='links facebook_icon'></span></a>
-			<a href='http://gplus.to/PopcornTimeTV' data-toggle="tooltip" data-placement="top" title="gplus.to/PopcornTimeTV" class='links google_icon'></span></a>
-			<a href='https://git.popcorntime.io'data-toggle="tooltip" data-placement="top" title="git.popcorntime.io" class='links stash_icon'></span></a>
-			<a href='http://blog.popcorntime.io' data-toggle="tooltip" data-placement="top" title="blog.popcorntime.io" class='links blog_icon'></span></a>
-			<a href='http://discuss.popcorntime.io' data-toggle="tooltip" data-placement="top" title="discuss.popcorntime.io" class='links forum_icon'></span></a>
+			<a href='https://popcorntime.io' data-toggle="tooltip" data-placement="top" title="https://popcorntime.io" class='links site_icon'></span></a>
+			<a href='https://twitter.com/popcorntimetv' data-toggle="tooltip" data-placement="top" title="https://twitter.com/popcorntimetv" class='links twitter_icon'></span></a>
+			<a href='https://www.fb.com/PopcornTimeTv' data-toggle="tooltip" data-placement="top" title="https://www.fb.com/PopcornTimeTv" class='links facebook_icon'></span></a>
+			<a href='https://gplus.to/PopcornTimeTV' data-toggle="tooltip" data-placement="top" title="https://gplus.to/PopcornTimeTV" class='links google_icon'></span></a>
+			<a href='https://github.com/popcorn-official'data-toggle="tooltip" data-placement="top" title="https://github.com/popcorn-official" class='links github_icon'></span></a>
+			<a href='http://blog.popcorntime.io' data-toggle="tooltip" data-placement="top" title="http://blog.popcorntime.io" class='links blog_icon'></span></a>
+			<a href='http://discuss.popcorntime.io' data-toggle="tooltip" data-placement="top" title="http://discuss.popcorntime.io" class='links forum_icon'></span></a>
 		</div>
 		<div class="last-line">
 			<%= i18n.__("Made with") %> <span style="color:#e74c3c;">&#10084;</span> <%= i18n.__("by a bunch of geeks from All Around The World") %>

Gruntfile.js
@@ -74,8 +74,8 @@ module.exports = function(grunt) {
 			var gitBranch = path.basename(gitRef);
 			if(grunt.file.exists('.git/' + gitRef)) {
 				var currCommit = grunt.file.read('.git/' + gitRef).trim();
-				pkgJson.git = {branch: gitBranch, commit: currCommit};
-				grunt.file.write('package.json', JSON.stringify(pkgJson, null, '  '));
+				var git = {branch: gitBranch, commit: currCommit};
+				grunt.file.write('.git.json', JSON.stringify(git, null, '  '));
 			}
 		}
 	});

src/app/app.js
@@ -82,6 +82,12 @@ App.db = Database;
 App.advsettings = AdvSettings;
 App.settings = Settings;
 
+fs.readFile('./.git.json', 'utf8', function(err, json) {
+    if(!err) {
+        App.git = JSON.parse(json);
+    }
+});
+
 App.addRegions({
     Window: '.main-window-region'
 });

commit 12ccbde450881f5a33752e4d6853161f546906a7

Fixed merge issue introduced in 17c395f + link uniformity
- git.popcorntime.io got turned back into github due to the merge conflict.
- Made them all use http instead of some using http and some https, feel free to re-change. But stuck to one please.

src/app/templates/about.tpl
@@ -18,13 +18,13 @@
 			</div>
 		</div>
 		<div class="icons_social">
-			<a href='https://popcorntime.io' data-toggle="tooltip" data-placement="top" title="https://popcorntime.io" class='links site_icon'></span></a>
-			<a href='https://twitter.com/popcorntimetv' data-toggle="tooltip" data-placement="top" title="https://twitter.com/popcorntimetv" class='links twitter_icon'></span></a>
-			<a href='https://www.fb.com/PopcornTimeTv' data-toggle="tooltip" data-placement="top" title="https://www.fb.com/PopcornTimeTv" class='links facebook_icon'></span></a>
-			<a href='https://gplus.to/PopcornTimeTV' data-toggle="tooltip" data-placement="top" title="https://gplus.to/PopcornTimeTV" class='links google_icon'></span></a>
-			<a href='https://github.com/popcorn-official'data-toggle="tooltip" data-placement="top" title="https://github.com/popcorn-official" class='links github_icon'></span></a>
-			<a href='http://blog.popcorntime.io' data-toggle="tooltip" data-placement="top" title="http://blog.popcorntime.io" class='links blog_icon'></span></a>
-			<a href='http://discuss.popcorntime.io' data-toggle="tooltip" data-placement="top" title="http://discuss.popcorntime.io" class='links forum_icon'></span></a>
+			<a href='http://popcorntime.io' data-toggle="tooltip" data-placement="top" title="http://popcorntime.io" class='links site_icon'></span></a>
+			<a href='http://twitter.com/PopcornTimeTV' data-toggle="tooltip" data-placement="top" title="twitter.com/PopcornTimeTV" class='links twitter_icon'></span></a>
+			<a href='http://www.fb.com/PopcornTimeTV' data-toggle="tooltip" data-placement="top" title="fb.com/PopcornTimeTV" class='links facebook_icon'></span></a>
+			<a href='http://gplus.to/PopcornTimeTV' data-toggle="tooltip" data-placement="top" title="gplus.to/PopcornTimeTV" class='links google_icon'></span></a>
+			<a href='http://git.popcorntime.io'data-toggle="tooltip" data-placement="top" title="git.popcorntime.io" class='links stash_icon'></span></a>
+			<a href='http://blog.popcorntime.io' data-toggle="tooltip" data-placement="top" title="blog.popcorntime.io" class='links blog_icon'></span></a>
+			<a href='http://discuss.popcorntime.io' data-toggle="tooltip" data-placement="top" title="discuss.popcorntime.io" class='links forum_icon'></span></a>
 		</div>
 		<div class="last-line">
 			<%= i18n.__("Made with") %> <span style="color:#e74c3c;">&#10084;</span> <%= i18n.__("by a bunch of geeks from All Around The World") %>